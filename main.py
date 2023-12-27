3062b5d8d56203a79830f10facc2c511830223f0
DaniilBornosuz <76657727+DaniilBornosuz@users.noreply.github.com>
2023-12-27 15:25:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,7 @@ conn.commit()
 class dialog(StatesGroup):
     send_info = State()
     chosie_task = State()
-    send_contact_dev = State()
-    send_contact_win = State()
-    send_contact_IB = State()
-    send_contact_lin = State()
+    send_contact = State()
 
 class MyCallback(CallbackData, prefix="my"):
     spec: 'str'
@@ -53,13 +50,16 @@ class MyCallback(CallbackData, prefix="my"):
 class push_button(CallbackData, prefix="fuck"):
     click: 'str'
 
+markup_request = ReplyKeyboardMarkup(resize_keyboard=True).add(
+KeyboardButton('/start', request_contact=True)).add
+
 @dp.message(Command('start'))
 async def start(message: Message):
     builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(text="Девы и разработка",callback_data=MyCallback(spec="dev",num="200").pack())),
-    builder.add(types.InlineKeyboardButton(text="Администрирование Windows", callback_data=MyCallback(spec="win",num="300").pack())),
-    builder.add(types.InlineKeyboardButton(text="Практические задания Linux/bsd", callback_data=MyCallback(spec="lin",num="000").pack())),
-    builder.add(types.InlineKeyboardButton(text="Проекты ИБ Безопасность", callback_data=MyCallback(spec="ib",num="100").pack())),
+    builder.add(types.InlineKeyboardButton(text="Девы и разработка",callback_data=MyCallback(spec="dev",num="20").pack())),
+    builder.add(types.InlineKeyboardButton(text="Администрирование Windows", callback_data=MyCallback(spec="win",num="30").pack())),
+    builder.add(types.InlineKeyboardButton(text="Практические задания Linux/bsd", callback_data=MyCallback(spec="lin",num="00").pack())),
+    builder.add(types.InlineKeyboardButton(text="Проекты ИБ Безопасность", callback_data=MyCallback(spec="ib",num="10").pack())),
     builder.add(types.InlineKeyboardButton(text="Просто Практика", callback_data='loh'))
     await message.answer("Выбери направление",reply_markup=builder.as_markup())
 
@@ -70,13 +70,13 @@ async def sev(query: CallbackQuery, callback_data: MyCallback):
     dir = callback_data.spec
     name = callback_data.num
     list = os.listdir(dir)
-    print(list)
-    with open(f"{callback_data.spec}/{name}{a}.txt") as file:
+    print(name)
+    with open(f"{callback_data.spec}/{name}0{a}.txt") as file:
         task = file.read()
         print(task)
         global builder
     builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(text="Берусь", callback_data=push_button(click='yes').pack()))
+    builder.add(types.InlineKeyboardButton(text="Берусь",request_contact=True, callback_data=push_button(click='yes').pack()))
     builder.add(types.InlineKeyboardButton(text="<<", callback_data=push_button(click='<<').pack()))
     builder.add(types.InlineKeyboardButton(text=">>", callback_data=push_button(click='>>').pack()))
     builder.add(types.InlineKeyboardButton(text="назад",callback_data=push_button(click='end').pack()))
@@ -92,24 +92,35 @@ async def button(query: CallbackQuery,callback_data: push_button):
     #for i in range(len(list)):
     if (callback_data.click == ">>"):
         a = random.randint(1, a)
-        """
-        ни как не мог вспомнить ка делать блядский перебор вот и поставил рандом 
-        кому не нравится идите нахуй мой бот че хочу то и делаю 
-        захочу назову переменную хуй
-        """
         print(a)
-        with open(f"{dir}/{name}{a}.txt") as file:
+        with open(f"{dir}/{name}0{a}.txt") as file:
             task = file.read()
         await query.message.edit_text(text=task,reply_markup=builder.as_markup())
+        if (callback_data.click == "yes"):
+            await query.send_contact(ADMIN, first_name=query.contact.first_name, last_name=query.contact.last_name,
+                                   phone_number=query.contact.phone_number)
+            await query.send_message(ADMIN, text=f"{task}")
     if (callback_data.click == "<<"):
         a = a+1
         with open(f"{dir}/{name}{a}.txt") as file:
             task = file.read()
         await query.message.edit_text(text=task,reply_markup=builder.as_markup())
+        if (callback_data.click == "yes"):
+            await query.send_contact(ADMIN, first_name=query.contact.first_name, last_name=query.contact.last_name,
+                                   phone_number=query.contact.phone_number)
+            await query.send_message(ADMIN, text=f"{task}")
+            #await dialog.send_contact()
     if (callback_data.click == "end"):
         await query.message.answer("Для открытия меню отправьте /start")
         await query.message.delete()
 
+"""
+@dp.message(state=dialog.send_contact, content_types=types.ContentType.CONTACT)
+async def proc(message: types.Message, state: FSMContext):
+    await bot.send_contact(OLEG, first_name=message.contact.first_name, last_name=message.contact.last_name, phone_number=message.contact.phone_number)
+    await bot.send_message(OLEG, text="")
+    await state.finish()
+"""
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
